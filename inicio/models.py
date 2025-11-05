a20a5142b96d4314b36fee2263ccc3c2a736ec33
Cristian D Biancotti <cdbiancotti@gmail.com>
2025-11-04 21:02:37 -0300

    primera app

---
+++ inicio/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Auto(models.Model):
+    modelo = models.CharField(max_length=30)
+    marca = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return f'Auto ({self.id}): {self.marca} - {self.modelo}'
